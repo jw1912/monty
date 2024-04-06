4d5e8853f7d0d61587c834f6f461e3496d09cbf4
JacquesRW <jamie.whiting2003@icloud.com>
2024-04-06 11:25:29 +0100

    add Shatranj

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use monty::UciLike;
 
-#[cfg(feature = "ataxx")]
-use monty::ataxx;
-
-#[cfg(not(feature = "ataxx"))]
-use monty::chess;
 
 fn main() {
     let mut args = std::env::args();
@@ -12,21 +7,34 @@ fn main() {
 
     #[cfg(not(feature = "ataxx"))]
     {
-        if let Some("bench") = arg1.as_deref() {
-            chess::Uci::bench(6);
-            return;
+        #[cfg(not(feature = "shatranj"))]
+        {
+            if let Some("bench") = arg1.as_deref() {
+                monty::chess::Uci::bench(6);
+                return;
+            }
+
+            monty::chess::Uci::run();
         }
 
-        chess::Uci::run();
+        #[cfg(feature = "shatranj")]
+        {
+            if let Some("bench") = arg1.as_deref() {
+                monty::shatranj::Uci::bench(6);
+                return;
+            }
+
+            monty::shatranj::Uci::run();
+        }
     }
 
     #[cfg(feature = "ataxx")]
     {
         if let Some("bench") = arg1.as_deref() {
-            ataxx::Uai::bench(5);
+            monty::ataxx::Uai::bench(5);
             return;
         }
 
-        ataxx::Uai::run();
+        monty::ataxx::Uai::run();
     }
 }
