9ed76127dd9461ea339b91fa95b7fbcd973d60b3
JacquesRW <jamie.whiting2003@icloud.com>
2024-04-06 23:50:13 +0100

    Value Score of dev vs main: 509 - 403 - 87  [0.553] 999 ...      dev playing White: 262 - 192 - 45  [0.570] 499 ...      dev playing Black: 247 - 211 - 42  [0.536] 500 ...      White vs Black: 473 - 439 - 87  [0.517] 999 Elo difference: 37.0 +/- 20.7, LOS: 100.0 %, DrawRatio: 8.7 % SPRT: llr 2.89 (100.1%), lbound -2.25, ubound 2.89 - H1 was accepted Bench: 1434963

--- src/chess.rs
+++ src/chess.rs
@@ -6,9 +6,7 @@ mod moves;
 mod policy;
 
 use crate::{
-    comm::UciLike,
-    game::{GameRep, GameState},
-    MctsParams,
+    comm::UciLike, game::{GameRep, GameState}, value::{ValueFeatureMap, ValueNetwork}, MctsParams
 };
 
 use self::frc::Castling;
@@ -20,6 +18,15 @@ pub use self::{
 };
 
 const STARTPOS: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+static VALUE: ValueNetwork<768, 16> =
+    unsafe { std::mem::transmute(*include_bytes!("../resources/chess-value001.bin")) };
+
+impl ValueFeatureMap for Board {
+    fn value_feature_map<F: FnMut(usize)>(&self, f: F) {
+        self.map_features(f);
+    }
+}
 
 pub struct Uci;
 impl UciLike for Uci {
@@ -123,7 +130,10 @@ impl GameRep for Chess {
     }
 
     fn get_policy_feats(&self) -> goober::SparseVector {
-        self.board.get_features()
+        //let mut feats = goober::SparseVector::with_capacity(32);
+        //self.board.map_features(|feat| feats.push(feat));
+        //feats
+        goober::SparseVector::with_capacity(0)
     }
 
     fn get_policy(&self, _: Self::Move, _: &goober::SparseVector) -> f32 {
@@ -131,19 +141,17 @@ impl GameRep for Chess {
     }
 
     fn get_value(&self) -> i32 {
-        const VALS: [i32; 5] = [100, 300, 300, 500, 900];
-        let boys = self.board.boys();
-        let opps = self.board.opps();
+        //const VALS: [i32; 5] = [100, 300, 300, 500, 900];
+        //let boys = self.board.boys();
+        //let opps = self.board.opps();
+        //let mut mat = 0;
+        //for (bb, val) in self.bbs().iter().skip(2).zip(VALS).take(5) {
+        //    let b = (bb & boys).count_ones();
+        //    let o = (bb & opps).count_ones();
+        //    mat += val * (b as i32 - o as i32);
+        //}
 
-        let mut mat = 0;
-
-        for (bb, val) in self.bbs().iter().skip(2).zip(VALS).take(5) {
-            let b = (bb & boys).count_ones();
-            let o = (bb & opps).count_ones();
-            mat += val * (b as i32 - o as i32);
-        }
-
-        mat
+        VALUE.eval(&self.board)
     }
 
     fn perft(&self, depth: usize) -> u64 {
--- train/value/src/bin/chess.rs
+++ train/value/src/bin/chess.rs
@@ -3,7 +3,7 @@ use bullet::{
     TrainingSchedule, WdlScheduler,
 };
 
-const HIDDEN_SIZE: usize = 8;
+const HIDDEN_SIZE: usize = 16;
 
 fn main() {
     let mut trainer = TrainerBuilder::default()
@@ -21,10 +21,10 @@ fn main() {
         eval_scale: 400.0,
         ft_regularisation: 0.0,
         batch_size: 16_384,
-        batches_per_superbatch: 128,
+        batches_per_superbatch: 512,
         start_superbatch: 1,
         end_superbatch: 40,
-        wdl_scheduler: WdlScheduler::Constant { value: 1.0 },
+        wdl_scheduler: WdlScheduler::Constant { value: 0.5 },
         lr_scheduler: LrScheduler::Step {
             start: 0.001,
             gamma: 0.1,
