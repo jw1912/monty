2604013c9867aec30a382357a7dc63af8ab4a125
JacquesRW <jamie.whiting2003@icloud.com>
2024-04-11 15:17:04 +0100

    Bench: 253963

--- train/value/src/bin/chess.rs
+++ train/value/src/bin/chess.rs
@@ -3,7 +3,7 @@ use bullet::{
     TrainingSchedule, WdlScheduler,
 };
 
-const HIDDEN_SIZE: usize = 128;
+const HIDDEN_SIZE: usize = 256;
 
 fn main() {
     let mut trainer = TrainerBuilder::default()
